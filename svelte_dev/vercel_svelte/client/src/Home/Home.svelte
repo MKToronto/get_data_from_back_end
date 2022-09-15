4c2132c5285880fa503e54f7e543f36c31401a6f
mkendal <mkendal@fortresstechnology.com>
2022-09-15 16:37:09 -0400

    fghgf

--- svelte_dev/vercel_svelte/client/src/Home/Home.svelte
+++ svelte_dev/vercel_svelte/client/src/Home/Home.svelte
@@ -4,7 +4,7 @@
   let get_value_to_send_interval;
   onMount(async () => {
     get_value_to_send();
-    make_new_get_value_to_send_interval(100)
+    make_new_get_value_to_send_interval(100);
   });
 
   onDestroy(() => {
@@ -13,51 +13,48 @@
   });
 
   async function get_value_to_send(slider_choice) {
-    const response = await fetch("https://get-data-python.vercel.app/get_value_to_send");
+    const response = await fetch(
+      "https://get-data-python.vercel.app/get_value_to_send"
+    );
     const get_value_to_send_response = await response.json();
     $value_to_send = get_value_to_send_response.value_to_send;
   }
-  import {
-    Slider,RadioButtonGroup
-  } from "smelte";
+  import { Slider, RadioButtonGroup } from "smelte";
 
   // async function send_time_sleep_to_back_end(slider_value_back_end){
   //   console.log("send_time_sleep_to_back_end", slider_value_back_end)
   //   const response = await fetch("https://get-data-python.vercel.app/send_time_sleep_to_back_end/"+slider_value_back_end);
   //   const get_value_to_send_response = await response.json();
 
-
   // }
 
-  async function send_slider_choice_to_back_end(slider_choice){
-    console.log("send_slider_choice_to_back_end", slider_choice)
-    const response = await fetch("https://get-data-python.vercel.app/send_slider_choice_to_back_end/"+slider_choice);
+  async function send_slider_choice_to_back_end(slider_choice) {
+    console.log("send_slider_choice_to_back_end", slider_choice);
+    const response = await fetch(
+      "https://get-data-python.vercel.app/send_slider_choice_to_back_end/" +
+        slider_choice
+    );
     const get_value_to_send_response = await response.json();
-
-
   }
 
-  async function make_new_get_value_to_send_interval(slider_value_front_end){
-    console.log("make_new_get_value_to_send_interval", slider_value_front_end)
+  async function make_new_get_value_to_send_interval(slider_value_front_end) {
+    console.log("make_new_get_value_to_send_interval", slider_value_front_end);
     clearInterval(get_value_to_send_interval);
     get_value_to_send_interval = setInterval(function () {
       get_value_to_send();
     }, slider_value_front_end);
-
-
   }
 
   let slider_value_back_end;
-  $: slider_value_back_end = 100
+  $: slider_value_back_end = 100;
 
   let slider_value_front_end;
-  $: slider_value_front_end = 100
+  $: slider_value_front_end = 100;
 
   let slider_choice;
-  $: slider_choice = "Digits"
+  $: slider_choice = "Digits";
   // let value2 = 0;
   let disabled = false;
-
 </script>
 
 <div class="mt-20">
@@ -66,68 +63,66 @@
       <p class="text-2xl">Welcome to a really great FastAPI and Svelte demo!</p>
     </div>
     <div class="col-start-5 col-end-8 ">
-      
       <p class="text-2xl">Value Received From FastAPI:</p>
     </div>
-      <div class="col-start-8 col-end-9 ">
+    <div class="col-start-8 col-end-9 ">
       <p class="text-blue-400 text-2xl">{$value_to_send}</p>
     </div>
-    
-      <div class="grid grid-cols-12 gap-y-4">
-        <div class="col-start-4 col-end-10 ">
-<h6>Choose a character type:</h6>
-</div>
-
-<div class="col-start-4 col-end-5 text-center">
-      <label>
-          <input
-              
-              bind:group={slider_choice}
-              on:change={send_slider_choice_to_back_end(slider_choice)}
-              type="radio"
-              value="Digits"
-          />
-          <h6>Digits</h6>
-      </label>
+  </div>
+  <div class="grid grid-cols-12 gap-y-4">
+    <div class="col-start-4 col-end-10 ">
+      <h6>Choose a character type:</h6>
     </div>
-      <div class="col-start-5 col-end-6 text-center ">
+
+    <div class="col-start-4 col-end-5 text-center">
       <label>
-          <input
+        <input
           bind:group={slider_choice}
           on:change={send_slider_choice_to_back_end(slider_choice)}
-              type="radio"
-              value="Letters"
-          />
-          <h6>Letters</h6>
+          type="radio"
+          value="Digits"
+        />
+        <h6>Digits</h6>
+      </label>
+    </div>
+    <div class="col-start-5 col-end-6 text-center ">
+      <label>
+        <input
+          bind:group={slider_choice}
+          on:change={send_slider_choice_to_back_end(slider_choice)}
+          type="radio"
+          value="Letters"
+        />
+        <h6>Letters</h6>
       </label>
     </div>
     <div class="col-start-6 col-end-7 text-center">
       <label>
-      <input
-      bind:group={slider_choice}
-      on:change={send_slider_choice_to_back_end(slider_choice)}
+        <input
+          bind:group={slider_choice}
+          on:change={send_slider_choice_to_back_end(slider_choice)}
           type="radio"
           value="Punctuation"
+        />
+        <h6>Punctuation</h6>
+      </label>
+    </div>
+  </div>
+  <div class="grid grid-cols-12 gap-y-20">
+    <div class="col-start-4 col-end-10 ">
+      <h6>Number of Milliseconds Between Updates:</h6>
+
+      <small>Value: {slider_value_front_end}</small>
+
+      <Slider
+        min="100"
+        max="2000"
+        color="blue"
+        bind:value={slider_value_front_end}
+        {disabled}
+        on:change={() =>
+          make_new_get_value_to_send_interval(slider_value_front_end)}
       />
-      <h6>Punctuation</h6>
-  </label>
+    </div>
   </div>
 </div>
-    <div class="col-start-4 col-end-10 ">
-
-      <h6>Number of Milliseconds Between Updates:</h6>
-      
-      <small>Value: {slider_value_front_end}</small>
-      
-      <Slider min="100" max="2000" color="blue" bind:value={slider_value_front_end} {disabled} on:change={() => make_new_get_value_to_send_interval(slider_value_front_end)} />
-      
-      
-      
-          </div>
-  </div>
-    </div>
-
-
-
-
-    
