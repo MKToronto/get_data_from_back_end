1b90cf83602ddaa3377539b5ae546ef69f29048f
mkendal <mkendal@fortresstechnology.com>
2022-09-15 00:38:41 -0400

    new t

--- svelte_dev/client/public/build/bundle.js
+++ svelte_dev/client/public/build/bundle.js
@@ -6164,8 +6164,10 @@ var app = (function () {
     var app = new App({
     	target: document.body,
     	headers: {
-            'Access-Control-Allow-Origin': '*' // or, e.g. replacing * by http://localhost:8000
-        }
+            'Access-Control-Allow-Credentials': 'true',
+    		'Access-Control-Allow-Origin': '*',
+    		'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    		'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',    }
     });
 
     return app;
--- svelte_dev/client/src/main.js
+++ svelte_dev/client/src/main.js
@@ -3,8 +3,10 @@ import App from './App.svelte';
 var app = new App({
 	target: document.body,
 	headers: {
-        'Access-Control-Allow-Origin': '*' // or, e.g. replacing * by http://localhost:8000
-    }
+        'Access-Control-Allow-Credentials': 'true',
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+		'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',    }
 });
 var stores;
 export default app;
